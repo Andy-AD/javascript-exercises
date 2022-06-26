832685da6302a930852331872485dd9c32d8ba88
Andrii Dovhaliuk <andy87@live.ru>
2022-06-26 19:33:37 +0400

    Complete 3 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const splitText = text.split('');
+    let result = '';
+    for (i = splitText.length - 1; i>=0; i--) {
+        result += splitText[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
