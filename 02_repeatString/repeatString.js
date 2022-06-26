dae40ff02278b55b4cf52aa5844984e33623ff8f
Andrii Dovhaliuk <andy87@live.ru>
2022-06-26 19:26:13 +0400

    Complete 2 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, quantity) {
+    let result = '';
+    if (quantity < 0) return "ERROR";
+    for (i=0; i < quantity; i++) {
+        result += text;
+    }
+    return result;
 };
 
 // Do not edit below this line
