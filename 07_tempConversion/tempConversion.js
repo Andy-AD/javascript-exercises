ad1a1ba112d2aa5c1b927dfaa62b7f99910bcb8a
Andrii Dovhaliuk <andy87@live.ru>
2022-06-28 12:46:45 +0400

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  let temperatureInC = (temperature - 32) * 5 / 9;
+  return checkIfHasDecimal(temperatureInC) ? +temperatureInC.toFixed(1) : temperatureInC;
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let temperatureInF = temperature * 9 / 5 + 32;
+  return checkIfHasDecimal(temperatureInF) ? +temperatureInF.toFixed(1) : temperatureInF;
 };
+
+function checkIfHasDecimal(number) {
+  return ((number - Math.floor(number)) !== 0)
+} 
 
 // Do not edit below this line
 module.exports = {
