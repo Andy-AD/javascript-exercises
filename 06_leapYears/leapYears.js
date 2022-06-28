ad7c7082264c33fc9e20f084cd2ddbcc6b54285c
Andrii Dovhaliuk <andy87@live.ru>
2022-06-28 12:05:07 +0400

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let dividedBy4 = year % 4;
+    let divideBy100 = year % 100;
+    let dividedBy400 = year % 400;
+    if (divideBy100 === 0 && dividedBy400 === 0) {
+        return true;
+    } else if ( dividedBy4 === 0 && divideBy100 !== 0) {
+        return true;
+    } else return false;
 };
 
 // Do not edit below this line
