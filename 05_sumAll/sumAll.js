290a1556516e74cc3fc5d7b316febd27bff03ec9
Andrii Dovhaliuk <andy87@live.ru>
2022-06-28 11:30:27 +0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(startNumber, endNumber) {
+    let sum = 0;
+    let isNumber = Number.isInteger(startNumber) && Number.isInteger(endNumber);
+    if (!isNumber) {
+        return "ERROR";
+    } else if (startNumber < 0 || endNumber < 0) {
+        return "ERROR";
+    } else {
+        if (startNumber > endNumber) {
+            sum = startNumber * (startNumber + 1) / 2;
+        } else {
+            sum = endNumber * (endNumber + 1) /2;
+        }
+        
+    }
+    return sum;
 };
 
 // Do not edit below this line
