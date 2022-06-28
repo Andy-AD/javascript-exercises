79a4431161b8c5c52a3872fda08315837e459d11
Andrii Dovhaliuk <andy87@live.ru>
2022-06-28 10:44:19 +0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...argumentsToRemove) {
+    const result = [];
+    for (const item of array) {
+        let counter = 0;
+        for (const argumentToRemove of argumentsToRemove) {
+            if (item === argumentToRemove) counter++;            
+        }
+        if (!counter) result.push(item);
+    }
+    return result;
 };
 
 // Do not edit below this line
