5775af7c8a95c72fed21c83f2fb9e9b14e2bf5c4
Andrii Dovhaliuk <andy87@live.ru>
2022-07-06 23:29:49 +0400

    Complete exercise and pass tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) { 
+     let newString = string.toLowerCase().replace(/[,.\s!?]/g, '');
+     let reverseString = newString.split('').reverse().join('');
+    return newString === reverseString;
+     
 };
 
 // Do not edit below this line
