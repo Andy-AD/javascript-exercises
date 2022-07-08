8ee0c4bb1f3a9a8cb1e9f11ca144cb64d65868de
Andrii Dovhaliuk <andy87@live.ru>
2022-07-08 12:57:05 +0400

    Complete exercise and pass test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrayOfPeople) {
+    const oldestMan = arrayOfPeople.reduce((oldest, person) => {
+        let age;
+        let ageOfOldest;
 
+        if (oldest.yearOfDeath) {
+            ageOfOldest = oldest.yearOfDeath - oldest.yearOfBirth;
+        } else {
+            let currentYear = (new Date()).getFullYear();
+            ageOfOldest = currentYear - oldest.yearOfBirth;
+        }
+
+        if (person.yearOfDeath) {
+            age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            let currentYear = (new Date()).getFullYear();
+            age = currentYear - person.yearOfBirth;
+        }
+
+        return (ageOfOldest <= age) ? person : oldest;
+    }, arrayOfPeople[0]);
+    return oldestMan;
 };
 
 // Do not edit below this line
