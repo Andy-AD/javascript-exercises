44da7477b7e6f5221341956049ca29ee5ce47a0b
Andrii Dovhaliuk <andy87@live.ru>
2022-07-08 12:10:17 +0400

    Complete exercise and pass tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let fib = [1,1];
+    if (number < 0) {
+        return "OOPS";
+    } else if (number <= fib.length) {
+        return fib[number-1];
+    }
+    for (let i = 1; i < number; i++) {
+        nextFibNumber = fib[i] + fib[i-1];
+        fib.push(nextFibNumber);
+    }
+    return fib[number - 1];
 };
 
 // Do not edit below this line
